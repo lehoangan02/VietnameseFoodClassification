77f3190523ae8552faea6eab8c92000e4b494739
lehoangan02 <146849112+lehoangan02@users.noreply.github.com>
2025-01-13 00:40:34 -0800

    Normalize Differently

--- food_dataset.py
+++ food_dataset.py
@@ -10,8 +10,10 @@ class VietnameseFoodDataset(Dataset):
         self.image_labels = pd.read_csv(annotations_file)
         self.img_dir = img_dir
         self.transform = transform or transforms.Compose([
-            transforms.Resize((224, 224)),
-            transforms.ToTensor()
+            transforms.Resize(256),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
         ])
         self.target_transform = target_transform
         # Debug print
